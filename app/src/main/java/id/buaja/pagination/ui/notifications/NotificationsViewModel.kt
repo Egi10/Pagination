45eef1deeea54b5f9ed083698378d118c799673f
wowbid <egi@wowbid.live>
2020-04-18 12:34:49 +0700

    Initial commit

---
+++ app/src/main/java/id/buaja/pagination/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package id.buaja.pagination.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
